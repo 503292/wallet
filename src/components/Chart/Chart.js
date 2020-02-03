ff497a4b2cb60777bb14cba492de534a861c2d34
viccyviccy <iamviccyviccy@gmail.com>
2020-02-03 19:01:03 +0200

    Table and Chart components created

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Chart.module.css';
+
+const Chart = () => {
+  return (
+    <div className={s.chart_main_div}>
+      <h2>hi there I`m Chart thing</h2>
+    </div>
+  );
+};
+
+export default Chart;
---
+++ src/components/Table/Table.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Table.module.css';
+
+const Table = () => {
+  return (
+    <div className={s.table_main_div}>
+      <h2>hi there I`m Table thing</h2>
+    </div>
+  );
+};
+
+export default Table;
