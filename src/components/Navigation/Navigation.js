9ebda771b219c19b07e956eb465e94e59f79f4a3
kalysh_maxym <503292@ukr.net>
2020-02-04 12:50:59 +0200

    ADD _ style for desctop Header, Navigation, Tabs

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -11,52 +11,47 @@ const Navigation = () => {
 
   // console.log(, 'window.screen.width');
   return (
-    <ul className={css.wrapNav}>
-      <li>
-        <NavLink
-          className={css.linkHome}
-          to="/home"
-          exact
-          activeClassName={css.selected}
-        >
-          <div className={css.wrapIcon}>
-            <Home className={`${css.iconHome} ${css.icons}`} />
-            {widthDevice >= 768 && <div className={css.descr}>Главная</div>}
-          </div>
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          className={css.linkDiagram}
-          to="/diagram"
-          exact
-          activeClassName={css.selected}
-        >
-          <div className={css.wrapIcon}>
-            <Diagram className={`${css.iconDiagram} ${css.icons}`} />
-            {widthDevice >= 768 && <div className={css.descr}>Статистика</div>}
-          </div>
-        </NavLink>
-      </li>
-      {widthDevice <= 1023 && (
-        <li>
-          <NavLink
-            className={css.linkCurrency}
-            to="/currency"
-            exact
-            activeClassName={css.selected}
-          >
-            <div className={css.wrapIcon}>
-              <Currency className={`${css.iconCurrency} ${css.icons}`} />
+    <div className={css.wrapNav}>
+      <NavLink
+        className={css.linkHome}
+        to="/home"
+        exact
+        activeClassName={css.selected}
+      >
+        <div className={css.wrapIcon}>
+          <Home className={`${css.iconHome} ${css.icons}`} />
+          {widthDevice >= 768 && <div className={css.descr}>Главная</div>}
+        </div>
+      </NavLink>
+      <NavLink
+        className={css.linkDiagram}
+        to="/diagram"
+        exact
+        activeClassName={css.selected}
+      >
+        <div className={css.wrapIcon}>
+          <Diagram className={`${css.iconDiagram} ${css.icons}`} />
+          {widthDevice >= 768 && <div className={css.descr}>Статистика</div>}
+        </div>
+      </NavLink>
 
-              {widthDevice >= 768 && (
-                <div className={css.descr}>Баланс: 24 000.00грн</div>
-              )}
-            </div>
-          </NavLink>
-        </li>
+      {widthDevice <= 1023 && (
+        <NavLink
+          className={css.linkCurrency}
+          to="/currency"
+          exact
+          activeClassName={css.selected}
+        >
+          <div className={css.wrapIcon}>
+            <Currency className={`${css.iconCurrency} ${css.icons}`} />
+
+            {widthDevice >= 768 && (
+              <div className={css.descr}>Баланс: 24 000.00грн</div>
+            )}
+          </div>
+        </NavLink>
       )}
-    </ul>
+    </div>
   );
 };
 
