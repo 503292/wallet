a6999ac9324daa8c3328e106fca6fd7f0e9221a2
viccyviccy <iamviccyviccy@gmail.com>
2020-02-04 13:35:08 +0200

    changes with the husky

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Pie } from 'react-chartjs-2';
+// import DiagramSample from '../../assets/diagram_sample.png';
 import s from './Chart.module.css';
 
-const Chart = () => {
+const ChartItem = () => {
+  const [chartData, setChartData] = useState({
+    labels: [
+      'Основные расходы',
+      'Продукты',
+      'Машина',
+      'Забота о себе',
+      'Забота о детях',
+      'Товары для дома',
+      'Образование',
+      'Досуг',
+      'Другие расходы',
+    ],
+    datasets: [
+      {
+        label: 'population',
+        data: [1235, 5687, 4788, 0, 25, 4587, 6587, 1, 568],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+      },
+    ],
+  });
+
   return (
     <div className={s.chart_main_div}>
-      <h2>hi there I`m Chart thing</h2>
+      <Pie
+        data={chartData}
+        width={320}
+        height={350}
+        options={{ maintainAspectRatio: false }}
+      />
+      {/* <img className={s.diagram_sample} src={DiagramSample} alt="diagram" /> */}
+      {/* <myChart width="400" height="400" /> */}
     </div>
   );
 };
 
-export default Chart;
+export default ChartItem;
