c4a751229c3537554d2763b1b5ec74bb434ae6a8
kalysh_maxym <503292@ukr.net>
2020-02-04 10:35:15 +0200

    REFACT _ style for Navigation icons-select

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,16 +4,7 @@ import { ReactComponent as Home } from '../../assets/icons/home/baseline-home-24
 import { ReactComponent as Diagram } from '../../assets/icons/diagrams/baseline-timeline-24px.svg';
 import { ReactComponent as Currency } from '../../assets/icons/currency exchange/baseline-attach_money-24px.svg';
 
-// import home from '../../assets/icons/home/baseline-home-24px.svg';
-// import diagram from '../../assets/icons/diagrams/baseline-timeline-24px.svg';
-// import currency from '../../assets/icons/currency exchange/baseline-attach_money-24px.svg';
-
 import css from './Navigation.module.css';
-
-const ativeStyle = {
-  color: 'tomato',
-  fill: 'red',
-};
 
 const Navigation = () => {
   return (
@@ -23,10 +14,10 @@ const Navigation = () => {
           className={css.linkHome}
           to="/home"
           exact
-          activeStyle={ativeStyle}
+          activeClassName={css.selected}
         >
           <Home className={`${css.iconHome} ${css.icons}`} />
-          {/* <img className={css.icons} src={home} alt="Главная" /> */}
+
           {/* <div>Главная</div> */}
         </NavLink>
       </li>
@@ -35,10 +26,10 @@ const Navigation = () => {
           className={css.linkDiagram}
           to="/diagram"
           exact
-          activeStyle={ativeStyle}
+          activeClassName={css.selected}
+          // activeStyle={ativeStyle}
         >
           <Diagram className={`${css.iconDiagram} ${css.icons}`} />
-          {/* <img className={css.icons} src={diagram} alt="Статистика" /> */}
           {/* <div>Статистика</div> */}
         </NavLink>
       </li>
@@ -47,10 +38,10 @@ const Navigation = () => {
           className={css.linkCurrency}
           to="/currency"
           exact
-          activeStyle={ativeStyle}
+          activeClassName={css.selected}
+          // activeStyle={ativeStyle}
         >
           <Currency className={`${css.iconCurrency} ${css.icons}`} />
-          {/* <img className={css.icons} src={currency} alt="Курс" /> */}
           {/* <div>Баланс</div> */}
         </NavLink>
       </li>
