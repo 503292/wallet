481ea8d1fb89aca0f2fbeb7624525cd89f70a461
viccyviccy <iamviccyviccy@gmail.com>
2020-02-05 15:07:32 +0200

    changes and fresh updates to styles

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,43 +4,6 @@ import { Pie } from 'react-chartjs-2';
 import s from './Chart.module.css';
 
 const ChartItem = ({ chartData }) => {
-  // const [chartData] = useState({
-  //   labels: [
-  //     'Основные расходы',
-  //     'Продукты',
-  //     'Машина',
-  //     'Забота о себе',
-  //     'Забота о детях',
-  //     'Товары для дома',
-  //     'Образование',
-  //     'Досуг',
-  //     'Другие расходы',
-  //   ],
-  //   options: {
-  //     legend: {
-  //       display: false,
-  //       position: 'bottom',
-  //     },
-  //   },
-  //   datasets: [
-  //     {
-  //       label: 'wallet',
-  //       data: [1235, 5687, 4788, 3200, 2500, 4587, 6587, 1561, 5618],
-  //       backgroundColor: [
-  //         '#ecb22a',
-  //         '#e28b20',
-  //         '#d25925',
-  //         '#67b7d0',
-  //         '#5593d7',
-  //         '#3e6ba8',
-  //         '#9cc254',
-  //         '#73ad57',
-  //         '#507c3a',
-  //       ],
-  //     },
-  //   ],
-  // });
-
   return (
     <div className={s.chart_main_div}>
       <Pie
