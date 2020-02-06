49dda360683f22b0388e56829e7a3fa1e16a288c
kalysh_maxym <503292@ukr.net>
2020-02-06 11:41:21 +0200

    ADD _ redux finance

---
+++ src/redux/finance/financeSelectors.js
@@ -0,0 +1,5 @@
+// export const getFinance = state => state.finance;
+
+export const getFinanceData = state => state.finance.data;
+export const getFinanceTotalBalance = state => state.finance.totalBalance;
+export const getFinanceTypeBalance = state => state.finance.typeTotalBalance;
